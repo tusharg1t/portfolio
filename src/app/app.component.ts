288a619f5d8c463d6c0815d3822681c332ec4320
Tushar Ranjan Singh <tranjansingh@lnw.com>
2025-02-06 01:30:54 +0530

    GOD SPEED =)

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-portfolio-app';
+}
